fb2a5720b74ddab35e963bdff2275f85eb56f05e
suziro2/try <suzir72@gmail.com>
2024-09-23 18:17:18 +0800

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+# user Scan Example (Enter a value)
+a=int(input("Enter a value of a:"))
+b=int(input("Enter a value of b:"))
+c=a+b
+e=a*b
+f=a-b
+g=float(a/b)
+print("A:",a)
+print("B:",b)
+print("Sum",c)
+print("Mul",e)
+print("Sub",f)
+print("Div",g)
+
